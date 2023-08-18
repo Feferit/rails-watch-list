213591e92ee5a9a5c1e82a27e57fa83787c32fe3
ferit zengin <feritzengin72300@icloud.com>
2023-08-18 14:15:07 +0200

    commit

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,14 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @bookmark.list = @bookmark
+    @bookmark.movie = @movie
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-20.times do
+Movie.destroy_all
+5.times do
   movie = Movie.new(
     title: Faker::Movie.title,
     overview: Faker::Lorem.sentence,
     poster_url: Faker::Internet.url,
-    rating: (0.1..10.0)
+    rating: rand(0.0..10.0)
   )
   movie.save!
 end
