450e205c4d9a90e60556520ef0e3077702aac176
ferit zengin <feritzengin72300@icloud.com>
2023-08-21 09:38:43 +0200

    commit

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,10 +5,21 @@ class BookmarksController < ApplicationController
   end
 
   def create
-    movie = Movie.find(params[:bookmark][:movie_id])
-    list = List.find(params[:list_id])
-    @bookmark = Bookmark.new
-    @bookmark.list = @bookmark
+    @movie = Movie.find(params[:bookmark][:movie_id])
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
     @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to lists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
   end
 end
