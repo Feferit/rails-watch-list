213b45c91fb1e4be7e748ff2c0f6af82c66830fc
ferit zengin <feritzengin72300@icloud.com>
2023-08-24 08:12:34 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
+
+  root 'pages#home'
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
